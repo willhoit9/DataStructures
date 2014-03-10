7f5e11d8228172aef0ece67682d59edf66dfc066
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-03-10 12:37:14 -0500

    on to something

--- Program2/src/program2/Set.java
+++ Program2/src/program2/Set.java
@@ -26,7 +26,25 @@ public class Set implements StringSet, Cloneable{
         head = new Node(null);
         size = 0;
     }
-       
+      
+    public void printSet(){
+        Node current = head;             
+        while(current != null) {
+                System.out.println(current.getData());
+                current = current.getLink();
+        }
+    }
+    
+    @Override
+    public String toString() {
+        Node current = head.getLink();
+        String output = "";
+        while(current != null) {
+            output += "[" + current.getData() + "]";     
+            current = current.getLink();
+        }
+        return output;
+    }
     
     @Override
     public void resize(int larger) {
@@ -34,16 +52,46 @@ public class Set implements StringSet, Cloneable{
             throw new IllegalArgumentException("\rAn Illegal Argument Exeption has Occured. \n"
 					+  " The list may only be resized larger than its current size.");
         }    
+        size = larger;
     }
 
     @Override
     public void insert(String entry) {
-        
+        if (!this.contains(entry)) {
+            
+            Node temp = new Node(entry);
+            Node current = head;
+            while (current.getLink() != null) {           
+                current = current.getLink();
+            }
+            current.setLink(temp);
+            resize(size + 1);  
+        }
     }
 
     @Override
     public void remove(String target) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Node current = head;
+        while (current.getLink() != null)
+        {
+            if (current.data != null && current.data.equalsIgnoreCase(target))
+            {
+                System.out.println("----TARGET DESTROYED");
+                current.setLink(current.getLink().getLink());
+                // size--;
+                // i don't think i should decrement size here because we want
+                // size to be an account of all of the total size of Set
+                // I changed the link to pass over the "removed" node, but it
+                // was not really deleted. private int size keeps track of all nodes
+                // regardless of removal from the links
+            }
+            
+            current = current.getLink();
+            
+            
+        }
+               
     }
 
     @Override
@@ -53,27 +101,55 @@ public class Set implements StringSet, Cloneable{
 
     @Override
     public String getFirstItem() {
+        
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean contains(String target) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean answer = false;
+        Node cursor;
+        if (head.link == null)
+            return false;
+ 
+        for (cursor = head; cursor != null; cursor = cursor.getLink()) {           
+            if (cursor.data != null && cursor.data.equalsIgnoreCase(target)){
+                answer = true;
+            }    
+        }
+        return answer;
     }
 
     @Override
     public boolean is_empty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return head.getLink() == null;
     }
 
+    // postcondition: returns total number of Strings currently in set
     @Override
-    public int inventory() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int inventory() { 
+        int answer = 0;
+        if (head.link == null)
+            return 0;
+ 
+        for (Node cursor = head; cursor != null; cursor = cursor.getLink()) {
+            if (cursor.data != null)
+                answer++;
+        }
+        return answer;
     }
 
-    @Override
+    @Override // postcondition: returns total size of Set (used & unused portions)
     public int getCapacity() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        int answer = 0;
+// 
+//        for (Node cursor = head; cursor != null; cursor = cursor.getLink()) {
+//                answer++;
+//        }
+//        return answer;
+        return size;
     }
     
     
@@ -82,63 +158,129 @@ public class Set implements StringSet, Cloneable{
     public static void main(String[] args) {
         
         
+        Set set = new Set();
+        System.out.println("\n Inventory?");
+        System.out.println(set.inventory());
         
-        boolean fileNotOpen = true;
-      //  Scanner kb = new Scanner(System.in);
-        String input;
-        Scanner inFile;
-        FileInputStream in = null;
+        System.out.println("\n Is empty?");
+        System.out.println(set.is_empty());
         
-       
-        while (fileNotOpen){
-        try {
-         //   This reads in the current program source code in my Netbeans 
-         //   project. If it it run outside of a project structure, replace with: in = new FileInputStream("src\\program2\\Set.java");
-            
-            in = new FileInputStream("src\\program2\\Set.java");
-            fileNotOpen = false;
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't open file; Goodbye");
-            System.exit(1);
-        }
-        } // end while
-        inFile = new Scanner(in);
+        System.out.println("\n printSet() method:");
+        set.printSet();
+        
+        System.out.println("\n ToString method:");
+        System.out.println(set.toString());
+        
+        System.out.println("\n Now adding strings:");
+        set.insert("john");
+        set.insert("paul");
+        set.insert("george");
+        set.insert("ringo");
+        set.insert("julian");
+        set.insert("nikolai");
+        set.insert("albert");
+        set.insert("fabrizio");
+        set.insert("paul");
+        set.insert("julian");
                 
-//        while (inFile.hasNextLine()) {
-//            input = inFile.nextLine();
-//            int end = input.length()-1;
-//            if (end > 0 && input.charAt(end) == ';') {
-//                System.out.println(input);
+        
+        System.out.println("\n Is empty?");
+        System.out.println(set.is_empty());
+        
+        System.out.println("\n Inventory?");
+        System.out.println(set.inventory() + " strings stored"); 
+        
+        System.out.println("\n Capacity?");
+        System.out.println(set.getCapacity() + "  is the total size of the Set"); 
+        
+        
+        String removeName = "ringo";
+        System.out.println("\n Removing: " + removeName);
+        set.remove(removeName);
+        
+        System.out.println("\n printSet() method:");
+        set.printSet();
+        
+        
+        System.out.println("\n ToString method:");
+        System.out.println(set);
+        
+        String containsName = "paul";
+        System.out.println("\n contains() method: " + containsName + "?");
+        System.out.println(set.contains(containsName));
+        
+        System.out.println("\n contains() method: " + removeName + "?");
+        System.out.println(set.contains(removeName));
+        
+        System.out.println("\n Inventory?");
+        System.out.println(set.inventory());
+        
+        System.out.println("\n Is empty?");
+        System.out.println(set.is_empty());
+        
+        System.out.println("\n Capacity?");
+        System.out.println(set.getCapacity() + "  is the total size of the Set");         
+        
+      //  set.remove("paul");
+        
+        
+        
+        
+//        boolean fileNotOpen = true;
+//        String input;
+//        Scanner inFile;
+//        FileInputStream in = null;
+//        
+//       
+//        while (fileNotOpen){
+//        try {
+//         //   This reads in the current program source code in my Netbeans 
+//         //   project. If it it run outside of a project structure, replace with: in = new FileInputStream("src\\program2\\Set.java");
+//            
+//            in = new FileInputStream("src\\program2\\Set.java");
+//            fileNotOpen = false;
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Couldn't open file; Goodbye");
+//            System.exit(1);
+//        }
+//        } // end while
+//        inFile = new Scanner(in);
 //                
+////        while (inFile.hasNextLine()) {
+////            input = inFile.nextLine();
+////            int end = input.length()-1;
+////            if (end > 0 && input.charAt(end) == ';') {
+////                System.out.println(input);
+////                
+////            }
+////        } // end while
+//        
+//        String [] words = null;
+//        while (inFile.hasNext()) {
+//            input = inFile.next().replaceAll("[^a-zA-Z ]", "");
+//            int end = input.length() -1;
+//            
+//            words = new String[300];
+//            int i = 0;
+//            if (end > 0) {
+//                System.out.println(input);
+//                words[i] = input;
+//                i = i + 1;
 //            }
 //        } // end while
-        
-        String [] words = null;
-        while (inFile.hasNext()) {
-            input = inFile.next().replaceAll("[^a-zA-Z ]", "");
-            int end = input.length() -1;
-            
-            words = new String[300];
-            int i = 0;
-            if (end > 0) {
-                System.out.println(input);
-                words[i] = input;
-                i = i + 1;
-            }
-        } // end while
-        
-        System.out.println(words.length);
-        
-      // if (words.length > 0) {
-            for (int i = 0; i < words.length; i++) {
-                System.out.println(words[i] + " -");
-            }
-      //  }
-
-        
-        
-        
-        inFile.close();
+//        
+//        System.out.println(words.length);
+//        
+//      // if (words.length > 0) {
+//            for (int i = 0; i < words.length; i++) {
+//                System.out.println(words[i] + " -");
+//            }
+//      //  }
+//
+//        
+//        
+//        
+//        inFile.close();
 
     } // end main()
     
@@ -159,35 +301,36 @@ public class Set implements StringSet, Cloneable{
     
         private class Node {
         
-        Node next;
+        Node link;
         String data;
         
         // Node constructor
         public Node(String _data) {
-            next = null;
             data = _data;
+            link = null;
         }
         
         // Node constructor for pointing to specific node
-        public Node(String _data, Node _next) {
-            next = _next;
+        public Node(String _data, Node _link) {
             data = _data;
+            link = _link;
         }
         
         public String getData() {
             return data;
+            // get the string from this node
         }
         
         public void setData(String _data) {
             data = _data;
         }
         
-        public Node getNext() {
-            return next;
+        public Node getLink() {
+            return link;
         }
         
-        public void setNext(Node _next) {
-            next = _next;
+        public void setLink(Node _link) {
+            link = _link;
         }
         
                 
