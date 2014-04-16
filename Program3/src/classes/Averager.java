dbd5a5f183e729c4f69776fca870472ed7e21c26
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-15 20:15:53 -0500

    halfway Program3

--- Program3/src/classes/Averager.java
+++ Program3/src/classes/Averager.java
@@ -4,11 +4,13 @@ public class Averager
 {
    private int count;  // How many numbers have been given to this averager
    private double sum; // Sum of all the numbers given to this averager                  
+   private int totalShowedUp; // the total amount of people that drive by
    
    public Averager( )
    {
        count =0;
        sum = 0;
+       totalShowedUp = 0;
    }
    
    public void addNumber(double value)
@@ -30,5 +32,17 @@ public class Averager
    public int howManyNumbers( )
    {
       return count;
+   }
+   
+   // a car shows up to the carwash
+   public void showUp()
+   {
+       totalShowedUp++;
+   }
+   
+   // how many people left because line was too long?
+   public int howManyLeft()
+   {
+       return totalShowedUp - count;
    }
 }
--- Program3/src/classes/CarWash.java
+++ Program3/src/classes/CarWash.java
@@ -10,19 +10,24 @@ public class CarWash
       Scanner kb = new Scanner (System.in);
       System.out.println("Enter wash time: ");
       int WASHTIME = kb.nextInt();
+      System.out.println("Enter wash cost: ");
+      double WASHCOST = kb.nextDouble(); 
       System.out.println("Enter the number of servers: ");
       int SERVERNUMBER = kb.nextInt();
+      System.out.println("Enter how long server breaks last: ");
+      int BREAKTIME = kb.nextInt();
       System.out.println("Enter arrival probability: ");
       double ARRIVALPROB = kb.nextDouble();
       System.out.println("enter time for simulation: ");
       int TOTALTIME = kb.nextInt();
       
-      carWashSimulate(WASHTIME, SERVERNUMBER, ARRIVALPROB, TOTALTIME);
+      carWashSimulate(WASHTIME, WASHCOST, SERVERNUMBER, BREAKTIME, ARRIVALPROB, TOTALTIME);
    }
     
    public static void carWashSimulate
-   (int washTime, int serverNumber, double arrivalProb, int totalTime)
+   (int washTime, double washCost, int serverNumber, int breakTime, double arrivalProb, int totalTime)
    {
+      int custID = 1;
       Queue<Integer> arrivalTimes = new LinkedList<Integer>( );  
       int next;
       Queue<Server> servers = new LinkedList<>();
@@ -36,6 +41,7 @@ public class CarWash
       Server machine;
       Averager waitTimes = new Averager( );
       int currentSecond;
+     // double moneyMade = 0;
   
       // Write the parameters to System.out.
       System.out.println("Seconds to wash one car: " + washTime);
@@ -45,15 +51,18 @@ public class CarWash
       System.out.println("Total simulation seconds: " + totalTime); 
    
       // Check the precondition:
-      if (washTime <= 0 || arrivalProb < 0 || arrivalProb > 1 || totalTime < 0 || serverNumber < 1)
+      if (washTime <= 0 || arrivalProb < 0 || arrivalProb > 1 || totalTime < 0 || serverNumber < 1 || breakTime < 0)
          throw new IllegalArgumentException("Values out of range"); 
 
       for (currentSecond = 0; currentSecond < totalTime; currentSecond++)
       {  // Simulate the passage of one second of time.
          // Check whether a new customer has arrived.
+         boolean arrived = false;
          if (arrival.query( )){                      // if new client comes this second...
+            arrived = true;
             arrivalTimes.add(currentSecond);         //
-            System.out.println("Customer arrived at " + currentSecond);
+            System.out.println("Customer "+ (custID++)  +" arrived at " + currentSecond);
+            waitTimes.showUp();
         }
          // Check whether we can start washing another car.
         if ((!servers.peek().isBusy( ))  &&  (!arrivalTimes.isEmpty( )))
@@ -64,9 +73,13 @@ public class CarWash
             next = arrivalTimes.remove( );
             waitTimes.addNumber(currentSecond - next);
             machine.start( );
-            System.out.println("Server " + machine.getName() + " started at " + currentSecond);
+            System.out.println("Server " + machine.getName() + " started "
+                    + "at " + currentSecond + " on Customer " + (custID-1) + "'s car.");
+       //     moneyMade = moneyMade + washCost;
             servers.add(machine);
-            machine = null;
+            // machine = null;
+         } else if (!arrivalTimes.isEmpty() & arrived == true) {
+             System.out.println("Customer "+ (custID-1)  +" left ");
          }
          // Subtract one second from the remaining time in the current wash cycle.
         
@@ -96,7 +109,13 @@ public class CarWash
       
       
       System.out.println("Customers served: " + waitTimes.howManyNumbers( )); 
-      if (waitTimes.howManyNumbers( ) > 0)
-         System.out.println("Average wait: " + waitTimes.average( ) + " sec");
+      if (waitTimes.howManyNumbers( ) > 0) {
+          System.out.println("Average wait: " + waitTimes.average( ) + " sec");
+          System.out.println("Customers who left because long line: " + waitTimes.howManyLeft());
+      }
+       System.out.println("Money made: $" + (waitTimes.howManyNumbers() * washCost));   
+       System.out.println("Potential not made because of impatient "
+               + "customers: $" + (waitTimes.howManyLeft() * washCost));
    } 
 }
+
