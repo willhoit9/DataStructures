8d2c0e083982563ccd95198877e3282429e0fdc5
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-16 09:44:12 -0500

    done with step 4

--- Program3/src/classes/CarWash.java
+++ Program3/src/classes/CarWash.java
@@ -14,6 +14,8 @@ public class CarWash
       double WASHCOST = kb.nextDouble(); 
       System.out.println("Enter the number of servers: ");
       int SERVERNUMBER = kb.nextInt();
+      System.out.println("Enter how much a server cost to run an hour: ");
+      double SERVERHOURLYCOST = kb.nextDouble();
       System.out.println("Enter how long server breaks last: ");
       int BREAKTIME = kb.nextInt();
       System.out.println("Enter arrival probability: ");
@@ -21,11 +23,11 @@ public class CarWash
       System.out.println("enter time for simulation: ");
       int TOTALTIME = kb.nextInt();
       
-      carWashSimulate(WASHTIME, WASHCOST, SERVERNUMBER, BREAKTIME, ARRIVALPROB, TOTALTIME);
+      carWashSimulate(WASHTIME, WASHCOST, SERVERNUMBER, SERVERHOURLYCOST, BREAKTIME, ARRIVALPROB, TOTALTIME);
    }
     
    public static void carWashSimulate
-   (int washTime, double washCost, int serverNumber, int breakTime, double arrivalProb, int totalTime)
+   (int washTime, double washCost, int serverNumber, double serverHourlycost, int breakTime, double arrivalProb, int totalTime)
    {
       int custID = 1;
       Queue<Integer> arrivalTimes = new LinkedList<Integer>( );  
@@ -33,7 +35,7 @@ public class CarWash
       Queue<Server> servers = new LinkedList<>();
        for (int i = 0; i < serverNumber; i++) {
            String name = "" + (i + 1);
-           Server s = new Server(washTime, name);
+           Server s = new Server(washTime, name, serverHourlycost);
            servers.add(s);
        }
       ClientGenerator arrival = new ClientGenerator(arrivalProb);
@@ -41,8 +43,11 @@ public class CarWash
       Server machine;
       Averager waitTimes = new Averager( );
       int currentSecond;
-     // double moneyMade = 0;
-  
+      double moneyMade = 0;
+      double moneyNotMade = 0;
+      double serversCostToday = 0;
+      boolean arrived = false;
+      
       // Write the parameters to System.out.
       System.out.println("Seconds to wash one car: " + washTime);
       System.out.println("Number or servers: " + serverNumber);
@@ -57,7 +62,7 @@ public class CarWash
       for (currentSecond = 0; currentSecond < totalTime; currentSecond++)
       {  // Simulate the passage of one second of time.
          // Check whether a new customer has arrived.
-         boolean arrived = false;
+         arrived = false;
          if (arrival.query( )){                      // if new client comes this second...
             arrived = true;
             arrivalTimes.add(currentSecond);         //
@@ -65,13 +70,15 @@ public class CarWash
             waitTimes.showUp();
         }
          // Check whether we can start washing another car.
-        if ((!servers.peek().isBusy( ))  &&  (!arrivalTimes.isEmpty( )))
+        if (arrived == true & (!servers.peek().isBusy( ))  &&  (!arrivalTimes.isEmpty( )))
          {
-            
+            // added arrive == true, because only 1 server can work on a 1 car at a time
+            // multiple servers cannot work on the same car.
             
             machine = servers.remove();
             next = arrivalTimes.remove( );
             waitTimes.addNumber(currentSecond - next);
+            
             machine.start( );
             System.out.println("Server " + machine.getName() + " started "
                     + "at " + currentSecond + " on Customer " + (custID-1) + "'s car.");
@@ -106,16 +113,34 @@ public class CarWash
       // Write the summary information about the simulation.
       
       
-      
+      moneyMade = (waitTimes.howManyNumbers() * washCost);
+      moneyNotMade = (waitTimes.howManyLeft() * washCost);
       
       System.out.println("Customers served: " + waitTimes.howManyNumbers( )); 
       if (waitTimes.howManyNumbers( ) > 0) {
           System.out.println("Average wait: " + waitTimes.average( ) + " sec");
           System.out.println("Customers who left because long line: " + waitTimes.howManyLeft());
       }
-       System.out.println("Money made: $" + (waitTimes.howManyNumbers() * washCost));   
+       System.out.println("Money made: $" + moneyMade);   
        System.out.println("Potential not made because of impatient "
-               + "customers: $" + (waitTimes.howManyLeft() * washCost));
+               + "customers: $" + moneyNotMade);
+       
+       
+       
+       for (Server server : servers) {
+          serversCostToday += server.costToday();
+           
+           System.out.println("Server " + server.getName() + " ran "
+                    + "" + server.getTimesRanToday()+ " times today");
+            System.out.println("Server " + server.getName() + " cost "
+                   + "$" + server.costToday() + " to run today");
+       }
+       System.out.println("Server Cost Today: $" + serversCostToday);
+       
+       System.out.println("Money made today, minus the cost of running "
+               + "those servers: $" + (moneyMade - serversCostToday)); 
+       
+       
    } 
 }
 
--- Program3/src/classes/Server.java
+++ Program3/src/classes/Server.java
@@ -1,20 +1,30 @@
 package classes;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Server
 {
    private int secondsForService; // Seconds for a single wash
    private int timeLeft;   // Seconds until this Server is no longer busy
+   
    // added to keep track of different servers - AW
    private String name;
    // added to allow servers to take breaks - AW
    private boolean onBreak;
+   // added to keep track of cost of operation - AW
+   private double hourlyCost;
+   private int timesRanToday;
    
-   public Server(int s, String n)
+   
+   public Server(int s, String name, double cost)
    {
-       secondsForService = s;
-       timeLeft =0;
-       name = n;
-       onBreak = false;
+       this.secondsForService = s;
+       this.timeLeft =0;
+       this.name = name;
+       this.hourlyCost = cost;
+       this.timesRanToday = 0;
+       this.onBreak = false;
    }
    
    public boolean isBusy( )
@@ -26,6 +36,14 @@ public class Server
         return onBreak;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getTimesRanToday() {
+        return timesRanToday;
+    }
+    
     public void setOnBreak(boolean onBreak) {
         this.onBreak = onBreak;
     }
@@ -36,14 +54,34 @@ public class Server
          timeLeft--;
    } 
 
-    public String getName() {
-        return name;
-    }
+   public double costToday()
+   {
 
+       double cost;
+       cost = ((timesRanToday * secondsForService)/3600f) * hourlyCost;
+
+       BigDecimal bd = new BigDecimal(cost);
+       bd = bd.setScale(2, RoundingMode.CEILING);
+       
+       cost = bd.doubleValue();
+
+       return cost;
+   }
+
+    
    public void start( )
    {
-      if (timeLeft > 0)
+      if (timeLeft > 0) {
          throw new IllegalStateException("Server is already busy.");
+      }
+      
       timeLeft = secondsForService;
+      
+      timesRanToday++;
    }  
+
+ 
+   
+   
+   
 }
